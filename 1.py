1b06ae2c6ae5d157e5abc6c21a13f5e6ae892a38
snikiforova <d6796055@gmail.com>
2023-04-28 17:08:30 +0300

    обновление 4

--- 1.py
+++ 1.py
@@ -1,37 +1,47 @@
 import csv
 import json
-
 from Otus2.files import BOOKS_FILE_PATH, USERS_FILE_PATH
 
+users = []
+with open(USERS_FILE_PATH) as f:
+    users = json.load(f)
 
-with open(USERS_FILE_PATH) as alfa:
-    users = json.load(alfa)
+users_list = []
+for u in users:
+    users_list.append({
+        'name': u['name'],
+        'gender': u['gender'],
+        'address': u['address'],
+        'age': u['age'],
+        'books': []
+    })
 
+books_list = []
+with open(BOOKS_FILE_PATH) as f:
+    rd = csv.DictReader(f)
+    for b in rd:
+        book = {
+            'title': b['Title'],
+            'author': b['Author'],
+            'pages': int(b['Pages']),
+            'genre': b['Genre']
+        }
+        books_list.append(book)
 
-with open(BOOKS_FILE_PATH) as alfa:
-    rd = csv.DictReader(alfa)
-    books_list = [beta for beta in rd]
-
-
+num_users = len(users_list)
 num_books = len(books_list)
-num_users = len(users)
 
-
-max_books = num_books // num_users
-rem_books = num_books % num_users
-
-
-distribution = [max_books] * num_users
-for c in range(rem_books):
-    distribution[c] += 1
-
+max_books_per_user = num_books // num_users
+remaining_books = num_books % num_users
 
 book_index = 0
-for c, user in enumerate(users):
-    user_books = books_list[book_index:book_index + distribution[c]]
-    user['books'] = user_books
-    book_index += distribution[c]
+for i, user in enumerate(users_list):
+    for j in range(max_books_per_user):
+        user['books'].append(books_list[book_index])
+        book_index += 1
+    if i < remaining_books:
+        user['books'].append(books_list[book_index])
+        book_index += 1
 
-
-with open('result.json', 'w') as alfa_out:
-    json.dump(users, alfa_out, indent=4)
+with open('result.json', 'w') as f:
+    json.dump(users_list, f, indent=4)
